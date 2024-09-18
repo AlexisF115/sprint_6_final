845a1ca5a6b8a433e335833fefd11698ecdf26c7
AlexisF115 <javier_alexis7@hotmail.com>
2024-09-17 21:49:34 -0700

    Proyecto6

---
+++ README.md
@@ -0,0 +1,13 @@
+# Librerias
+**Pandas
+**Plotly-express
+**Streamlit
+
+Requisitos
+**Python
+PROYECTO 6
+
+Funcionalidad
+**Histograma
+**Grafico de dispersión
+**Interactividad
---
+++ app.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import plotly_express as plt
+import streamlit as st
+
+car_data = pd.read_csv("vehicles_us.csv")
+st.header("Cuadro de mandos de anuncios de vehiculos")
+
+#Histograma
+hist_button = st.button('Construir histograma')      
+if hist_button: 
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+#Grafico de dispersión
+build_histogram = st.checkbox('Construir un histograma')
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    fig= px.scatter(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
